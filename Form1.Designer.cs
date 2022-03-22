4a9e470cb4e42b7837d1b5cff552e63195a2d4e9
Daniel fernandes <51752973+df7803@users.noreply.github.com>
2022-03-22 17:28:07 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace CAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkLST = new System.Windows.Forms.CheckBox();
+            this.checkGMT = new System.Windows.Forms.CheckBox();
+            this.checkGEO = new System.Windows.Forms.CheckBox();
+            this.checkDXF = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BUSCA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "HELP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(156, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 30);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(304, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(495, 436);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(487, 410);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DETALHISTA";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(4, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(480, 85);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "GERAR RELATORIO";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(1, 184);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(483, 115);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "DESEJA COPIAR OS ARQUIVOS PARA UMA PASTA LOCAL";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.SandyBrown;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(6, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(470, 23);
+            this.label5.TabIndex = 3;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(7, 35);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(470, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "ESCOLHA O LOCAL";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.checkLST);
+            this.groupBox1.Controls.Add(this.checkGMT);
+            this.groupBox1.Controls.Add(this.checkGEO);
+            this.groupBox1.Controls.Add(this.checkDXF);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(481, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MARQUE O TIPO DE ARQUIVO QUE DESEJA VERIFICAR";
+            // 
+            // checkLST
+            // 
+            this.checkLST.AutoSize = true;
+            this.checkLST.Location = new System.Drawing.Point(283, 37);
+            this.checkLST.Name = "checkLST";
+            this.checkLST.Size = new System.Drawing.Size(61, 24);
+            this.checkLST.TabIndex = 3;
+            this.checkLST.Text = ".LST";
+            this.checkLST.UseVisualStyleBackColor = true;
+            // 
+            // checkGMT
+            // 
+            this.checkGMT.AutoSize = true;
+            this.checkGMT.Location = new System.Drawing.Point(197, 36);
+            this.checkGMT.Name = "checkGMT";
+            this.checkGMT.Size = new System.Drawing.Size(67, 24);
+            this.checkGMT.TabIndex = 2;
+            this.checkGMT.Text = ".GMT";
+            this.checkGMT.UseVisualStyleBackColor = true;
+            // 
+            // checkGEO
+            // 
+            this.checkGEO.AutoSize = true;
+            this.checkGEO.Location = new System.Drawing.Point(93, 36);
+            this.checkGEO.Name = "checkGEO";
+            this.checkGEO.Size = new System.Drawing.Size(68, 24);
+            this.checkGEO.TabIndex = 1;
+            this.checkGEO.Text = ".GEO";
+            this.checkGEO.UseVisualStyleBackColor = true;
+            this.checkGEO.CheckedChanged += new System.EventHandler(this.checkGEO_CheckedChanged);
+            // 
+            // checkDXF
+            // 
+            this.checkDXF.AutoSize = true;
+            this.checkDXF.Location = new System.Drawing.Point(7, 37);
+            this.checkDXF.Name = "checkDXF";
+            this.checkDXF.Size = new System.Drawing.Size(65, 24);
+            this.checkDXF.TabIndex = 0;
+            this.checkDXF.Text = ".DXF";
+            this.checkDXF.UseVisualStyleBackColor = true;
+            this.checkDXF.CheckedChanged += new System.EventHandler(this.checkDXF_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(1, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(483, 107);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LOCAL ONDE ESTÃO OS ARQUIVOS";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.SandyBrown;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(7, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(470, 23);
+            this.label4.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(6, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(470, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "LOCAL DO ARQUIVO";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(487, 410);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(156, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 30);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(156, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 30);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkLST;
+        private System.Windows.Forms.CheckBox checkGMT;
+        private System.Windows.Forms.CheckBox checkGEO;
+        private System.Windows.Forms.CheckBox checkDXF;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string caminho  = @"Y:\004 - ENGENHARIA\06 - CAM";          // CAMINHO PARA IR DIRETO A PASTA DE CAM
+        private string excel = "planilha Excel files (*.excel) |*.xls";      //SELECIONA O EXCELL
+        private string geo = "trumph-GEO files (*.GEO) | *.GEO";
+        private string lst = "trumph-LST files (*.LST) | *.LST";
+        private string dxf = "trumph-DXF files (*.DXF) | *.DXF";
+        private string gmt = "trumph-GMT files (*.GMT) | *.GMT";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog planilha = new OpenFileDialog();
+            planilha.InitialDirectory = caminho;
+            planilha.Filter = excel;
+
+            /*ctrl + K + c*/
+            if (planilha.ShowDialog() == DialogResult.OK)
+            {
+                //System.Diagnostics.Process.Start(openFileDialog.FileName);
+                label1.Text = planilha.SafeFileName;
+                label1.BackColor = Color.Yellow;
+                // MessageBox.Show("arquivo existe");
+                //System.Diagnostics.Process.Start(openFileDialog.FileName);  //abre o arquivo selecionado
+                
+            }
+            else
+            {
+                label1.BackColor = Color.Green;
+                button1.BackColor = Color.Crimson;
+                button2.BackColor = Color.Cyan;
+                button3.BackColor = Color.Red;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = MessageBox.Show("pre requisitos- Planilha OSFA  ".ToUpper() + "\n" +
+           " 1_ o nome  do arquivo OSFA precisa ser igual ao nome" +
+           "do programa normalmente  e so remover o terceiro digito (-)" + "\n" + "\n" +
+           " Remover a coluna tipo.".ToUpper() + "\n" + "2_ Remover  o L antes do material usando somente na laser" + "\n" +
+              "\n" + "\n" +
+           "LEGENDA DE CORES" + "\n" + "3_ Vermelho = Revisão diferente ou não existe no programa/ peça" + "\n" +
+           "azul Quantidade diferente").ToString();
+        }
+
+        private void checkDXF_CheckedChanged(object sender, EventArgs e)
+        {
+            checkDXF.Select();
+            
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+           
+            DialogResult result = folder.ShowDialog();
+            
+            if (result == DialogResult.OK)
+            {
+                label4.Text = folder.SelectedPath;
+            }
+        }
+
+        private void checkGEO_CheckedChanged(object sender, EventArgs e)
+        {
+           checkGEO.Select();
+            
+        }
+    }
+}
